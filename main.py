285b92f577f49cf91c3024deb5cc45b45a953137
seongwon1121 <seongwon1121@mju.ac.kr>
2026-03-17 17:43:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 name1 = input("이름을 입력하시오: ")
 writing1 = int(input("당신의 글쓰기 점수를 입력하시오: "))
 python1 = int(input("당신의 파이썬 점수를 입력하시오: "))
-last_avg1 = float(input("당신의 지낙학기 평균을 입력하시오: "))
+last_avg1 = float(input("당신의 지난학기 평균을 입력하시오: "))
 sum = writing1 + python1
 average1 = sum / 2
 print(f"{name1} 학생의 글쓰기 점수는 {writing1}, 파이썬 점수는 {python1} 입니다")
